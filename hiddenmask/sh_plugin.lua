769e1aa05910cee165f2cb16cb913900f9c9d13e
stalkerhl2-dev <63262118+stalkerhl2-dev@users.noreply.github.com>
2020-04-16 11:47:07 +0200

    Added a small readme inside the plugin to help setting up
    Noone cares

--- hiddenmask/sh_plugin.lua
+++ hiddenmask/sh_plugin.lua
@@ -2,6 +2,19 @@ PLUGIN.name = "Mask"
 PLUGIN.author = "Stalker"
 PLUGIN.description = "Adds masks to hide your identity."
 
+--[[
+ Doesn't matter whats the item just make to have these in some kind of outfit file
+ 
+function ITEM:OnEquipped()
+    self.player:SetNetVar("isMasked",true)
+end
+ 
+function ITEM:OnUnequipped()
+    self.player:SetNetVar("isMasked",false)
+end
+
+--]]
+
 function PLUGIN:PopulateCharacterInfo(client, character, container)
     if client:GetNetVar("isMasked") then
         timer.Simple(0.1,function()
